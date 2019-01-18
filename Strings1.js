8603210286af9fe1d6764779810244f4adf9ef8a
sophrs <45198009+sophrs@users.noreply.github.com>
2019-01-18 08:41:28 +0000

    Add files via upload

---
+++ Strings1.js
@@ -0,0 +1,22 @@
+//string exercise 1
+let comment = "He said 'My name is Elliot'";
+document.write(comment.toUpperCase());
+
+
+//string exercise 2
+let string = "hello";
+let number = 5;
+document.write(string,number);
+
+//arrays1/strings 3
+
+let myArray = ["cat", "dog", "pig"];
+document.write(myArray);
+
+myArray[myArray.length] = "horse";
+document.write(myArray);
+
+document.write(myArray.splice(1));
+
+
+
